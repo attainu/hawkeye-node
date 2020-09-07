b15587c23a8cafe68ad0daed0baef45f86e9dcc2
Manish Mahant <manishmahant>
2020-09-07 08:43:43 +0530

    added validations

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import { body, validationResult } from 'express-validator';
 const upload = multer();
 const router = express.Router()
 
@@ -11,24 +12,41 @@ router.get('/',function(req,res) {
         address: 'Address 1',
         address2: 'Address 2',
         city: 'Toronto',
-        zip: 'ABC'
+        zip: 'ABC',
+        errors: {}
     }
     res.render('signup',initialData);
 });
 
-router.post('/', upload.array(), function(req,res) {
-    console.log('POST URL')
-    console.log('DATA',req.body);
-    
-    // here we will add validations
-    // According to the validation we can either show the form
-    // or take it to another page.
-    // if(req.body.email) {
-    //     res.redirect('/profile');
-    // }
-    // res.json({message:'User Added'})
-    res.render('signup',req.body);
-});
+router.post(
+    '/',
+    upload.array(),
+    [
+        // email must be an email
+        body('email').isEmail().withMessage('Please enter a valid email.'),
+        // password must be at least 5 chars long
+        body('password').isLength({ min: 5 }).withMessage('Password must have 5 characters.')
+    ],
+    function(req,res) {
+        const errors = validationResult(req);
+
+        const data = {
+            ...req.body,
+            errors: errors.mapped()
+        }
+
+        console.log('DATA',data);
+        
+        // here we will add validations
+        // According to the validation we can either show the form
+        // or take it to another page.
+        // if(req.body.email) {
+        //     res.redirect('/profile');
+        // }
+        // res.json({message:'User Added'})
+        res.render('signup',data);
+    }
+);
 
 // module.exports = Router;
 
